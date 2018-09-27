517236de588e47b91c92ae9927ae2a5aa3d75142
Shashi <shashikunal@gmail.com>
2018-09-27 21:41:07 +0530

    Add files via upload

---
+++ http.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app-http',
+  templateUrl: './http.component.html',
+  styleUrls: ['./http.component.css']
+})
+export class HttpComponent  {
+  posts:any[];
+  private url = "https://jsonplaceholder.typicode.com/posts";
+  constructor(private http : Http) {
+      http.get(this.url).subscribe((response)=>{
+              this.posts = response.json();
+      })
+   };
+
+   createData(input:HTMLInputElement){
+        let post = {title : input.value}
+        input.value = '';
+          this.http.post(this.url , JSON.stringify(post)).subscribe((response)=>{
+           post['id']= response.json().id;
+           this.posts.splice(0 , 0 , post)
+          });
+    }
+
+  
+}
